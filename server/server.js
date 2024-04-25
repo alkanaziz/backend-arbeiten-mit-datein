514b7259b6f383174377996fa49f100db0fd2607
alkanaziz <alkanaziz@gmail.com>
2024-04-25 09:51:29 +0200

    Update .gitignore in server directory and create server.js to allow to post from client

---
+++ server/server.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import fs from 'fs';
+import cors from 'cors';
+import { nanoid } from 'nanoid';
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const filePath = "data.json";
+
+
+app.get('/', (req, res) => {
+    res.send("Server is running");
+})
+
+app.post('/user', (req, res) => {
+    // check if the data.json file exists
+    if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, "[]");
+    }
+
+    const userInfo = req.body;
+    const newUserInfo = {
+        id: nanoid(),
+        ...userInfo
+    }
+
+    const data = fs.readFileSync(filePath, "utf8");
+    const jsonData = JSON.parse(data);
+    jsonData.push(newUserInfo);
+    fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+
+    res.send("User added successfully");
+})
+
+app.get('/users', (req, res) => {
+    if (!fs.existsSync(filePath)) {
+        res.send("No user found");
+        return;
+    }
+
+    const jsonData = fs.readFileSync(filePath, "utf8");
+    const jsObjData = JSON.parse(jsonData);
+    res.send(jsObjData);
+})
+app.listen(3002, () => {
+    console.log("Server is running on http://localhost:3002");
+})
