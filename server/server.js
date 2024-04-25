a6fb2517775ff891044ddc132a301366b737b8ed
alkanaziz <alkanaziz@gmail.com>
2024-04-25 22:39:26 +0200

    Refactor server.js to include authentication middleware and login endpoint

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { nanoid } from 'nanoid';
 
 
+const filePath = "data.json";
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -11,10 +12,54 @@ app.use(cors());
 app.use((req, res, next) => {
     console.log(`Anfrage an ${req.method} ${req.url}`);
     next();
+    // res.send("Ich bin Middleware nummer1 und habe die Anfrage abgebrochen");
 })
 
-const filePath = "data.json";
+app.use("/users" || "/", (req, res, next) => {
+    console.log("Ich bin Middleware nummer2");
+    if (req.method === "GET") {
+        console.log("GET Anfrage, erlaubt!");
+        next();
+    } else {
+        console.log("POST Anfrage, thou shall not pass!");
+        res.send("Nur GET Anfragen sind erlaubt!")
+    }
+})
 
+const users = [
+    {
+        "id": 12321,
+        "userName": "john050",
+        "role": "admin",
+        "password": "admin"
+    },
+    {
+        "id": 12322,
+        "userName": "jane050",
+        "role": "user",
+        "password": "user"
+    },
+    {
+        "id": 12323,
+        "userName": "joe050",
+        "role": "user",
+        "password": "user"
+    }
+]
+
+const authMiddleware = (req, res, next) => {
+    const { userName, role, password } = req.body.user;
+    const user = users.find(user => user.userName === userName && user.role === role && user.password === password)
+    if (user) {
+        next();
+    } else {
+        res.send("Login failed");
+    }
+};
+
+app.post('/login', authMiddleware, (req, res) => {
+    res.send("Login successful");
+});
 
 app.get('/', (req, res) => {
     res.send("Server is running");
